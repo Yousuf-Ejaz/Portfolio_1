04e881a7d6e92cb42bb15e4693b70b81b5c4a6e8
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-06-01 20:58:47 +0530

    add: Added Projects section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,50 @@
 export const projects = [
-  {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 0,
-  },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+	{
+		title: "House MarketPlace",
+		description:
+			"Built using React and Firebase. It is a full stack Application with all the CRUD operations. It can basically be used to post and view Listings. Furthermore, there can be two types of Listings: For Sale and for Rent. One can Login and view various available listings, listings presently on discount and also post their own listings ",
+		image: "/images/house-marketplace.png",
+		tags: ["React", "Firebase", "OAuth"],
+		visit: "https://github.com/Yousuf-Ejaz/house-marketplace",
+		source: "https://house-marketplace-ecru.vercel.app/",
+		id: 0,
+	},
+	{
+		title: "GitHub Finder",
+		description:
+			"Built using React, GitHub API, ContextAPI, TailwindCSS and DaisyUI. It is a frontend Application to view Github Users and display their details in personalised Manner.Used React.js for designing Frontend and ContextAPI for state management.Used Github API to fetch the relevant data. Used TailwindCSS along with DaisyUI to improve the UI. ",
+		image: "/images/github-finder.png",
+		tags: ["React", "GitHubAPI", "TailwindCSS", "DaisyUI"],
+		source: "https://github-finder-yousuf-ejaz.vercel.app/",
+		visit: "https://github.com/Yousuf-Ejaz/github-finder",
+		id: 1,
+	},
+	{
+		title: "Galeria",
+		description:
+			"Built using React, Firebase and TailwindCSS. Developed a social media application to impart better online presence for sketch artists and painters. Used React.js for designing Frontend and Google Firebase for adding Backend. Used Firebase Authentication for adding Authentication and Firebase Firestore for a realtime database.Used Tailwind CSS to achieve an edge over the UI design part.",
+		image: "/images/galeria.png",
+		tags: ["React", "TailwindCSS", "Firebase"],
+		source: "https://galeria-sh.netlify.app/",
+		visit: "https://github.com/Yousuf-Ejaz/Galeria",
+		id: 2,
+	},
+	{
+		title: "Feedback App",
+		description:
+			"It is a simple feedback Application built using ReactJS.One can add feedback, remove feedback or create a new feedback.",
+		image: "/images/feedback-app.png",
+		tags: ["React", "Javascript"],
+		source: "https://loquacious-salmiakki-cc01c9.netlify.app/",
+		visit: "https://github.com/Yousuf-Ejaz/feedback-application",
+		id: 3,
+	},
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+	{ year: 2017, text: "Started my journey" },
+	{ year: 2018, text: "Worked as a freelance developer" },
+	{ year: 2019, text: "Founded JavaScript Mastery" },
+	{ year: 2020, text: "Shared my projects with the world" },
+	{ year: 2021, text: "Started my own platform" },
 ];
