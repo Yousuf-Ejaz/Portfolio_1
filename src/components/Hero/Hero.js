2ab067d8782e0ed90de2e89f20ffa0ee0b975192
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-06-01 20:27:39 +0530

    add: Added Hero Component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+	<>
+		<Section row nopadding>
+			<LeftSection>
+				<SectionTitle main center>
+					Building
+					<br />
+					Elegant Web
+					<br />
+					Experiences
+				</SectionTitle>
+				<SectionText>
+					Hi! I'm Yousuf. I'm a Full Stack Web Developer and a Competitive Programmer.
+				</SectionText>
+				<Button onClick={props.handleClick}>About Me</Button>
+			</LeftSection>
+		</Section>
+	</>
 );
 
 export default Hero;
